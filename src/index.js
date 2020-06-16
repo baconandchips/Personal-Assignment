28f2f2286f3c875fdae54383de5cc18f292a9510
Guanting <16443280+baconandchips@users.noreply.github.com>
2020-06-15 19:45:25 -0700

    File cleanups

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import combineReducers from './reducers/index.js'; // we exported combineReducers
 
 ReactDOM.render(
-  <Provider store={createStore(combineReducers)}>
-    <App />
-  </Provider>,
+    <App />,
   document.getElementById('root')
 );
 
