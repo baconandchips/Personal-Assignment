08e43706589de4c7df71c4dc8e4856b8aca9f63b
Guanting <16443280+baconandchips@users.noreply.github.com>
2020-06-15 19:48:32 -0700

    I need better quality check going forwards

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -2,7 +2,7 @@ export const ADD_MESSAGE = 'ADD_MESSAGE'
 export const TOGGLE_MESSAGE = 'TOGGLE_MESSAGE'
 export const SET_FILTER = 'SET_FILTER'
 
-export const FILTER_ALL = 'all'
+export const FILTER_ALL = 'All'
 export const FILTER_SHOWHIDDEN = 'Expanded'
 export const FILTER_HIDDEN = 'Unexpanded'
 export const Filters = [FILTER_ALL, FILTER_SHOWHIDDEN, FILTER_HIDDEN]
