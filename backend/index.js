b37ba7bb0080274b67b6b670edb48516792f87f1
Guanting <16443280+baconandchips@users.noreply.github.com>
2020-07-21 19:33:34 -0700

    index move lines

--- backend/index.js
+++ backend/index.js
@@ -15,12 +15,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../frontend', 'build')));
+
 app.use('/', indexRouter);
 app.use('/messages', messagesRouter);
 
 //if (process.env.NODE_ENV === 'production') {
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '../frontend', 'build')));
+
 //}
 
 // app.use(express.static('../frontend/build'));
