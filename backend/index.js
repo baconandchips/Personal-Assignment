2358cd1f06ff8a49828a72c55cf799edf2044c96
Guanting <16443280+baconandchips@users.noreply.github.com>
2020-07-21 19:30:39 -0700

    index again

--- backend/index.js
+++ backend/index.js
@@ -20,13 +20,13 @@ app.use('/messages', messagesRouter);
 
 //if (process.env.NODE_ENV === 'production') {
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
+app.use(express.static(path.join(__dirname, '../frontend', 'build')));
 //}
 
 // app.use(express.static('../frontend/build'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
 })
 
 const uri = process.env.ATLAS_URI;
